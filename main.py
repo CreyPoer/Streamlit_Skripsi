51f55a424f44efbe4066d56203d12e8a6fced81b
SoniIndraM <103136259+CreyPoer@users.noreply.github.com>
2025-10-25 16:13:47 +0700

    Remove optional label for Pembimbing

--- main.py
+++ main.py
@@ -1080,7 +1080,7 @@ elif menu == "💡 Tentang Penelitian":
     - **Nama:** Soni Indra Maulana
     - **NIM:** 210411100136
     - **Institusi:** Universitas Trunojoyo Madura
-    - **Pembimbing:** (Opsional)
+    - **Pembimbing:** 
       - Pembimbing 1: Prof. Arif Muntasa, S.Si., M.T.
       - Pembimbing 2: Prof. Rima Tri Wahyuningrum, S.T., M.T.
     
